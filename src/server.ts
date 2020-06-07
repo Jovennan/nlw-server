b58c09a8d44621bff7a10c23423d58e2e15e04f4
Jovennan Ramalho <jovennan@gmail.com>
2020-06-07 15:47:57 -0300

    Aula 5 - 50% done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { request, response } from 'express'
 import cors from 'cors'
 import path from 'path'
+import { errors } from 'celebrate'
+
 import routes from './routes'
 
 const app = express()
@@ -11,5 +13,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333, '192.168.1.11')
